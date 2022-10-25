2936a85b61e91aeff357f132cc3a187c52113487
Christopher Payken <94931537+cpayken@users.noreply.github.com>
2022-10-25 12:22:40 -0700

    Update rand_key.py

--- rand_key.py
+++ rand_key.py
@@ -1,7 +1,12 @@
-from base64 import b64encode
-from os import urandom
+import secrets
+#from base64 import b64encode
+#from os import urandom
 
-random_bytes = urandom(64)
-random_key = b64encode(random_bytes).decode('utf-8')
+#random_bytes = urandom(64)
+#random_key = b64encode(random_bytes).decode('utf-8')
 
-print (random_key)
+#print (random_key)
+
+token = secrets.token_hex(64)
+
+print (token)
